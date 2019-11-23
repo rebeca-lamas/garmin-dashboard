96f1b2f0337e81dcca2cccdfd725ebda36d4a596
Hokie <52640930+rebeca-lamas@users.noreply.github.com>
2019-11-23 12:46:56 -0500

    restructuring

--- dashboard/assets/js/week-heatmap-logic.js
+++ dashboard/assets/js/week-heatmap-logic.js
@@ -1,3 +1,4 @@
+
 const margin = { top: 50, right: 0, bottom: 100, left: 30 },
 width = 960 - margin.left - margin.right,
 height = 430 - margin.top - margin.bottom,
@@ -8,7 +9,7 @@ colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8",
 days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
 times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 
-const svg = d3.select("#chart").append("svg")
+const svg = d3.select("#week-heatmap").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
@@ -45,7 +46,7 @@ const heatmapChart = function(csvFile) {
         };
     },
     function(error, data) {
-        const colorScale = d3.scaleQuantile()
+        const colorScale = d3.scale.quantile()
         .domain([0, buckets - 1, d3.max(data, function (d) { return d.values; })])
         .range(colors);
         
@@ -62,6 +63,9 @@ const heatmapChart = function(csvFile) {
         .attr("class", "hour bordered")
         .attr("width", gridSize)
         .attr("height", gridSize)
+        .style("fill", colors[0]);
+        
+        cards.transition().duration(1000)
         .style("fill", function(d) { return colorScale(d.values); });
         
         cards.select("title").text(function(d) { return d.values; });
@@ -69,27 +73,27 @@ const heatmapChart = function(csvFile) {
         cards.exit().remove();
         
         const legend = svg.selectAll(".legend")
-            .data([0].concat(colorScale.quantiles()), function(d) { return d; });
-
-        legend.enter().append("g")
-            .attr("class", "legend");
-
-        legend.enter().append("rect")
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height)
-            .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
-            .style("fill", function(d, i) { return colors[i]; });
+        .data([0].concat(colorScale.quantiles()), function(d) { return d; });
         
-        legend.enter().append("text")
-            .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height + gridSize);
+        legend.enter().append("g")
+        .attr("class", "legend");
+        
+        legend.append("rect")
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height)
+        .attr("width", legendElementWidth)
+        .attr("height", gridSize / 2)
+        .style("fill", function(d, i) { return colors[i]; });
+        
+        legend.append("text")
+        .attr("class", "mono")
+        .text(function(d) { return "≥ " + Math.round(d); })
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height + gridSize);
         
         legend.exit().remove();
         
     });  
 };
-const csv = '../../day-hour.csv';
+const csv = '../../resources/data/day-hour.csv';
 heatmapChart(csv); 

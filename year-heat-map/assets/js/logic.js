c586430a8a4b01701f9a1c25ee222b64fa4d6652
NHustle <51274056+natashacrosby@users.noreply.github.com>
2019-11-23 12:02:40 -0500

    Update logic.js

--- year-heat-map/assets/js/logic.js
+++ year-heat-map/assets/js/logic.js
@@ -1,19 +1,19 @@
-var width = 900,
+const width = 900,
     height = 105,
     cellSize = 12; // cell size
     week_days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
     month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 	
-var day = d3.time.format("%w"),
+const day = d3.time.format("%w"),
     week = d3.time.format("%U"),
     percent = d3.format(".1%"),
 	format = d3.time.format("%Y%m%d");
 	parseDate = d3.time.format("%Y%m%d").parse;
 		
-var color = d3.scale.linear().range(["white", '#002b53'])
+const color = d3.scale.linear().range(["white", '#002b53'])
     .domain([0, 1])
     
-var svg = d3.select(".calender-map").selectAll("svg")
+const svg = d3.select(".calender-map").selectAll("svg")
     .data(d3.range(2011, 2015))
   .enter().append("svg")
     .attr("width", '100%')
@@ -28,7 +28,7 @@ svg.append("text")
     .style("text-anchor", "middle")
     .text(function(d) { return d; });
  
-for (var i=0; i<7; i++)
+for (const i=0; i<7; i++)
 {    
 svg.append("text")
     .attr("transform", "translate(-5," + cellSize*(i+1) + ")")
@@ -37,7 +37,7 @@ svg.append("text")
     .text(function(d) { return week_days[i]; }); 
  }
 
-var rect = svg.selectAll(".day")
+const rect = svg.selectAll(".day")
     .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
   .enter()
 	.append("rect")
@@ -49,7 +49,7 @@ var rect = svg.selectAll(".day")
     .attr("fill",'#fff')
     .datum(format);
 
-var legend = svg.selectAll(".legend")
+const legend = svg.selectAll(".legend")
       .data(month)
     .enter().append("g")
       .attr("class", "legend")
@@ -74,9 +74,9 @@ d3.csv("jm_data.csv", function(error, csv) {
     d.value = parseInt(d.value);
   });
 
- var value_Max = d3.max(csv, function(d) { return d.value; });
+ const value_Max = d3.max(csv, function(d) { return d.value; });
  
-  var data = d3.nest()
+  const data = d3.nest()
     .key(function(d) { return d.Date; })
     .rollup(function(d) { return  Math.sqrt(d[0].value / value_Max); })
     .map(csv);
@@ -89,14 +89,14 @@ d3.csv("jm_data.csv", function(error, csv) {
 
 function numberWithCommas(x) {
     x = x.toString();
-    var pattern = /(-?\d+)(\d{3})/;
+    const pattern = /(-?\d+)(\d{3})/;
     while (pattern.test(x))
         x = x.replace(pattern, "$1,$2");
     return x;
 }
 
 function monthPath(t0) {
-  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+  const t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
       d0 = +day(t0), w0 = +week(t0),
       d1 = +day(t1), w1 = +week(t1);
   return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
